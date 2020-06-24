5b6a80659303fc892310caf6d8d85a5aaf0aa042
Natalia <Natalia@Monica>
2020-06-24 12:01:11 -0300

    Asunto:Inicializacion del Proyect Responsable:Monica Natalia Tapia Tareao1:Inicializacin del Proyecto Final

---
+++ src/main/java/ar/edu/unju/fi/ProyectoFinalGrupo13Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalGrupo13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalGrupo13Application.class, args);
+	}
+
+}
