3d29c523cc571bf17daf5a9cbdb60bccd7bd2e43
Dalila <Dalila@Daly>
2020-06-25 17:56:17 -0300

    Generacion de la clase Localidad y sus  constructores y métodos accesores.

---
+++ src/main/java/ar/edu/unju/fi/tracking/model/Localidad.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package ar.edu.unju.fi.tracking.model;
+
+/**
+ * Representa una localidad
+ * @author Dalila
+ *
+ */
+public class Localidad {
+	/**
+	 * Representa el nombre de una localidad o pueblo
+	 */
+	private String nombre;
+	//---------------CONTRUCTORES-------------------
+	/**
+	 * Constructor  por defecto
+	*/
+	public Localidad() {
+		// TODO Auto-generated constructor stub
+	}
+	//Constructor Parametrisado
+	/**
+	 * @param nombre
+	 */
+	public Localidad(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	//-------------METODOS ACCESORES-----------------
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return "Localidad [nombre=" + nombre + "]";
+	}
+	
+}
