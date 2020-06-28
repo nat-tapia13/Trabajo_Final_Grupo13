e65b853e04de5b5611b001f7305491796d337b83
Ema <Ema@192.168.0.10>
2020-06-28 00:17:50 -0300

    Se agrega los paquetes ar.edu.unju.fi.tracking.repository y ar.edu.unju.fi.tracking.service

--- src/main/java/ar/edu/unju/fi/ProyectoFinalGrupo13Application.java
+++ src/main/java/ar/edu/unju/fi/ProyectoFinalGrupo13Application.java
@@ -1,13 +1,24 @@
 package ar.edu.unju.fi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoFinalGrupo13Application {
+public class ProyectoFinalGrupo13Application implements CommandLineRunner{
+	
+	private static Logger LOG = LoggerFactory.getLogger(ProyectoFinalGrupo13Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoFinalGrupo13Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
