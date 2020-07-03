3f89e5e0e624253b5be8121295e9a0d085461afa
Dalila <Dalila@Daly>
2020-07-03 11:41:12 -0300

    Se realizo las anotaciones para el mapeo de la BD para la clase Usuario y se agrego la dependencia con la clase RegistroTracking

--- src/main/java/ar/edu/unju/fi/tracking/model/Usuario.java
+++ src/main/java/ar/edu/unju/fi/tracking/model/Usuario.java
@@ -2,6 +2,13 @@ package ar.edu.unju.fi.tracking.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,37 +17,52 @@ import org.springframework.stereotype.Component;
  * 
  */
 @Component("unUsuario")
+@Entity
+@Table(name = "usuarios")
 public class Usuario implements Serializable {
 
 	/*
 	 * ATRIBUTOS
 	 * 
 	 */
-	
+	/**
+	 * Representa la identificacion univoca de un Usuario
+	 */
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario")
+	private Long id;
 	/**
 	 * Atributo que representa el nombre del usuario
 	 */
+	@Column (name="nombre_usuario",length = 150, nullable = true)
 	private String nombreUsuario;
 	
 	/**
 	 * Atributo que representa la contraseña del usuario
 	 */
+	@Column (name= "password",length = 8,nullable = true)
 	private String password;
 	
 	/**
 	 * Atributo que representa el nombre real del usuaurio
 	 */
+	@Column (name="nombre_real",length = 150, nullable = true)
 	private String nombreReal;
 	
 	/**
 	 * Atributo que representa el apellido real del usuario
 	 */
+	@Column (name="apellido_real",length = 150, nullable = true)
 	private String apellidoReal;
 	
 	/**
 	 *Atributo que representa al tipo de usuario
 	 */
+	@Column (name="tipo_usuario",length = 11, nullable = true)
 	private String tipoUsuario;
+	
+	private List<RegistroTraiking>
 	
 	/*
 	 * CONSTRUCTORES
@@ -64,8 +86,14 @@ public class Usuario implements Serializable {
 	 * @param apellidoReal
 	 * @param tipoUsuario
 	 */
-	public Usuario(String nombreUsuario,String password,String nombreReal,String apellidoReal,String tipoUsuario) {
-		
+	public Usuario(String nombreUsuario, String password, String nombreReal, String apellidoReal,
+			String tipoUsuario) {
+		super();
+		this.nombreUsuario = nombreUsuario;
+		this.password = password;
+		this.nombreReal = nombreReal;
+		this.apellidoReal = apellidoReal;
+		this.tipoUsuario = tipoUsuario;
 	}
 	/*
 	 * METODOS ACCESORES
@@ -80,7 +108,6 @@ public class Usuario implements Serializable {
 	public String getNombreUsuario() {
 		return nombreUsuario;
 	}
-
 	/**
 	 * Asigna un valor al nombreUsuario a Usuario
 	 * @param nombreUsuario del nombreUsuario del Usuario
@@ -152,13 +179,25 @@ public class Usuario implements Serializable {
 	public void setTipoUsuario(String tipoUsuario) {
 		this.tipoUsuario = tipoUsuario;
 	}
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
 	@Override
 	public String toString() {
-		return "Usuario [nombreUsuario=" + nombreUsuario + ", password=" + password + ", nombreReal=" + nombreReal
-				+ ", apellidoReal=" + apellidoReal + ", tipoUsuario=" + tipoUsuario + ", getNombreUsuario()="
-				+ getNombreUsuario() + ", getPassword()=" + getPassword() + ", getNombreReal()=" + getNombreReal()
-				+ ", getApellidoReal()=" + getApellidoReal() + ", getTipoUsuario()=" + getTipoUsuario() + "]";
+		return "Usuario [nombreUsuario=" + nombreUsuario + ", password=" + password + ", nombreReal="
+				+ nombreReal + ", apellidoReal=" + apellidoReal + ", tipoUsuario=" + tipoUsuario + "]";
 	}
+	
 	
 	
 }
