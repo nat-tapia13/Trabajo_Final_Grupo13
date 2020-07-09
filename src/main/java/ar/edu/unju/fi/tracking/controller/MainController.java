54544e4378342e35f677251f17dddfac348913c4
Natalia <Natalia@Monica>
2020-07-09 00:57:57 -0300

    Asunto:Creamos el paquete de Controller,Modificaciones en el pom,header y footer. Autor:Monica Natalia Tapia Descripcion:Inyectamos dependencias de thymeleaf-extras-springsecurity5 al pom para trabajar con la vista del login.

---
+++ src/main/java/ar/edu/unju/fi/tracking/controller/MainController.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.tracking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/loginuser")
+	public String ingresar(Model model) {
+	return "userlogin" ;
+	
+}
+}
